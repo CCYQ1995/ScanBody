a9879d2e07dd385295315e697984598b19d2f0dd
CCYQ <chenyq@healthmall.com>
2018-08-16 14:40:10 +0800

    Initial Commit

---
+++ ScanBody/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ScanBody
+//
+//  Created by jkm on 2018/8/16.
+//  Copyright © 2018年 jkm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ScanBody/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ScanBody
+//
+//  Created by jkm on 2018/8/16.
+//  Copyright © 2018年 jkm. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
